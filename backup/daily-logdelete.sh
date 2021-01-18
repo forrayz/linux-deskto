771d0548b93b7dc911b63e4ca57acc3904cc3055
forrayz <forrayz@gmail.com>
2021-01-18 14:16:17 +0100

    daily

---
+++ backup/daily-logdelete.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+echo "############################################################"
+echo "#     daily-logdelete.sh      BASH scipt                   #"
+echo "#     author: Zoltan forray                                #"
+echo "#     contributor:      Z                                  #"
+echo "###################################################################"
+#trap "set +x; sleep 1; set -x" DEBUG
+
+# BASEDIR=/data/docker-data/volumes/
+BASEDIR=/home/forrayz/development/public/linux-desktop/backup
+
+FILE_PATTERN="*.log"
+SUBFOLDERS=$(find ${BASEDIR} -name $FILE_PATTERN -printf "%h\n" | sort -u)
+for ii in ${SUBFOLDERS}
+do
+    jj=${ii:${#BASEDIR}} #strip off BASEDIR, jj is the relative path
+
+            
+    find  $BASEDIR/ -name $FILE_PATTERN -type f -mtime +1 -exec rm -f {} \;
+    echo "$FILE_PATTERN found in ---------------> $BASEDIR$jj"  
+    
+done
+
+FILE_PATTERN="*.gz"
+SUBFOLDERS=$(find ${BASEDIR} -name $FILE_PATTERN -printf "%h\n" | sort -u)
+for ii in ${SUBFOLDERS}
+do
+    jj=${ii:${#BASEDIR}} #strip off BASEDIR, jj is the relative path
+
+            
+    find  $BASEDIR/ -name $FILE_PATTERN -type f -mtime +1 -exec rm -f {} \;
+    echo "$FILE_PATTERN found in ----------------> $BASEDIR$jj"  
+    
+done
+
+
+FILE_PATTERN="access.txt"
+SUBFOLDERS=$(find ${BASEDIR} -name $FILE_PATTERN -printf "%h\n" | sort -u)
+for ii in ${SUBFOLDERS}
+do
+    jj=${ii:${#BASEDIR}} #strip off BASEDIR, jj is the relative path
+
+            
+    find  $BASEDIR/ -name $FILE_PATTERN -type f -mtime +1 -exec rm -f {} \;
+    echo "$FILE_PATTERN found in ----------> $BASEDIR$jj"  
+    
+done
+
+
+
+
+
---
+++ backup/generate_log_for_test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+mkdir -p 1/1
+mkdir -p 1/2
+mkdir -p 1/3
+mkdir -p 1/4
+mkdir -p 1/5
+
+mkdir -p 2/1
+mkdir -p 2/2
+mkdir -p 2/3
+mkdir -p 2/4
+mkdir -p 2/5
+
+mkdir -p 3/1
+mkdir -p 3/2
+mkdir -p 3/3
+mkdir -p 3/4
+mkdir -p 3/5
+
+mkdir -p 4/1
+mkdir -p 4/2
+mkdir -p 4/3
+mkdir -p 4/4
+mkdir -p 4/5
+
+mkdir -p 5/1
+mkdir -p 5/2
+mkdir -p 5/3
+mkdir -p 5/4
+mkdir -p 5/5
+
+
+touch 3/valamilog.log
+touch 5/valamilog.log
+touch 1/1/valamilog.log
+touch 1/3/valamilog.log
+touch 1/4/valamilog.log
+
+
+
+
+
+touch 1/valamilog.log
+touch 3/valamilog.log
+touch 5/valamilog.log
+touch 1/1/valamilog.log
+touch 1/3/valamilog.log
+touch 1/4/valamilog.log
+
+touch 1/valamilog.gz
+touch 3/valamilog.gz
+touch 5/valamilog.gz
+touch 1/1/valamilog.gz
+touch 1/3/valamilog.gz
+touch 1/4/valamilog.gz
+
+
+
+touch 1/access.txt
+touch 3/access.txt
+touch 5/access.txt
+touch 1/1/access.txt
+touch 1/3/access.txt
+
+
