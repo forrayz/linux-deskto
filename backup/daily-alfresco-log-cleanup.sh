5ea6f081ec07c8b572d9abe2db3f1d8d55a01967
forrayz <forray.zoltan@ulyssys.hu>
2021-01-18 16:26:13 +0100

    oh catalina

--- backup/daily-alfresco-log-cleanup.sh
+++ backup/daily-alfresco-log-cleanup.sh
@@ -8,25 +8,24 @@ echo "#     author: Zoltan forray                                #"
 echo "#     contributor:      Z                                  #"
 echo "############################################################"
 #trap "set +x; sleep 1; set -x" DEBUG
-
+BASEDIR=/data/docker-data/volumes/
 
 LOGTIME=$(date)
 echo "daily-alfresco-log-cleanup.sh has been started against $BASEDIR at ----> $LOGTIME" >> /var/log/daily-alfresco-log-cleanup.sh.log
 
-BASEDIR=/data/docker-data/volumes/
 # BASEDIR=/home/forrayz/development/public/linux-desktop/backup
 
 
 ALFRESCO_FOLDERS=$(find $BASEDIR -type d -name alfresco-data)
 FILE_PATTERN1="*.log"
 FILE_PATTERN2="*.gz"
-FILE_PATTERN2="*.txt"
+FILE_PATTERN3="*.txt"
 
 for ii in ${ALFRESCO_FOLDERS}
 do
-        find  $ii/tomcat/logs/ -name $FILE_PATTERN -type f -mtime +2 -exec rm -f {} \;
         find  $ii/tomcat/logs/ -name $FILE_PATTERN1 -type f -mtime +2 -exec rm -f {} \;
         find  $ii/tomcat/logs/ -name $FILE_PATTERN2 -type f -mtime +2 -exec rm -f {} \;
+        find  $ii/tomcat/logs/ -name $FILE_PATTERN3 -type f -mtime +2 -exec rm -f {} \;
         echo "possible logfolder alfresco-data for pattern $FILE_PATTERN1,  $FILE_PATTERN2,  $FILE_PATTERN3 ---------------> $ $ii/tomcat/logs/"
     
 done
